1d0ff6a541c3f7a80cb6b9127eb0148ea491d52e
khabaroff <khabaroff@gmail.com>
2016-08-07 21:04:26 +0300

    localhost 80 deleted

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get('*', function (req, res) {
    res.sendFile('build/index.html', {root: __dirname});
 });
 
-app.listen(8000, function () {
-   console.log('Example app listening on port 8000!');
+app.listen(80, function () {
+   console.log('Example app listening on port 80!');
 });
