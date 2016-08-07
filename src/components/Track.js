0854e6a1ba761a899fb323289d3d61008bcce561
khabaroff <khabaroff@gmail.com>
2016-08-07 21:06:19 +0300

    localhost 80 deleted 2

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,18 +1,12 @@
 import React, {Component, PropTypes} from 'react'
 
 function Track(props) {
-  let btn = ''
-
-  if () {
-    btn = (<button onClick={() => {props.onAddTrack(props.id)}}>Add</button>)
-  }
-
   return (
       <div className="Track">
         <div className="Track-left">
           <div className="Track-number">{props.number + 1}.</div>
           <div className="Track-title" onClick={() => {props.onPlayTrack(props.id, props.previewUrl)}}>{props.title}</div>
-          {btn}
+           <button onClick={() => {props.onAddTrack(props.id)}}>Add</button>
         </div>
         <div className="Track-duration">{props.duration}</div>
       </div>
